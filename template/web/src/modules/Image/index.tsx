7c404ae2dab1743787d2aecb1c1f8e99b3e429a6
arnespremberg <mail@arnespremberg.com>
2021-10-05 13:02:50 +0200

    cleanup

--- template/web/src/modules/Image/index.tsx
+++ template/web/src/modules/Image/index.tsx
@@ -9,13 +9,6 @@ import SanityImage from '@/atoms/SanityImage'
 const ImageModule: FC<SanityModuleImage> = ({ image, ...props }) =>
   image?.asset ? (
     <>
-      {/* this is the old GatsbyImage implementation */}
-      {/* <GatsbyImage image={image?.asset?.gatsbyImageData} alt={image?.alt || ''} /> */}
-      {/* 
-        if we use SanityImage we have a littleless traffic during build
-        time but more traffic on runtime on our SanityCDN - more expensive
-        however hotspot will be supported out of the box.
-      */}
       <SanityImage image={image} />
       {image.subtitle && (
         <Box pt={1}>
@@ -34,21 +27,3 @@ fragment moduleImageData on SanityModuleImage {
   }
 }
 `
-
-
-// ...SanityImageWithPreview
-
-
-
-// export const query = graphql`
-//   fragment moduleImageData on SanityModuleImage {
-      // image {
-      //   ...Image
-      //   alt
-      //   subtitle
-      //   asset {
-      //     gatsbyImageData(width: 4096)
-      //   }
-      // }
-//   }
-// `
