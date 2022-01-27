705917f70200bc66658817937ece5adb68324dff
Anton Leitner <anton.b.leitner@gmail.com>
2022-01-27 12:42:57 +0100

    Move Scrolly RichText Block Content to RichText

--- template/web/src/modules/Scrolly/RichText/index.tsx
+++ template/web/src/modules/Scrolly/RichText/index.tsx
@@ -2,62 +2,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { FC } from 'react'
 import { graphql } from 'gatsby'
-import BlockContent from '@sanity/block-content-to-react'
-import Link from '@/atoms/Link'
-import Text from '@/atoms/Text'
 import Box from '@/atoms/Box'
-import { SanityButton } from '@/atoms/Button'
-
-const Block = (props: any): JSX.Element | null => {
-  const { style } = props.node
-  const variants = {
-    h1: 'head',
-    h2: 'head',
-    h3: 'subhead',
-    h4: 'subhead',
-    caption: 'caption',
-    p: 'normal',
-    span: 'normal',
-    normal: 'normal'
-  }
-  if (props.children) {
-    return (
-      <Text
-        as={style === 'normal' ? 'p' : style === 'caption' ? 'span' : style}
-        variant={variants[style]}
-      >
-        {props.children}
-      </Text>
-    )
-  } else return null
-}
-
-const serializers = {
-  types: {
-    h1: (props: any) => <Text as="h1" variant="head" {...props} />,
-    h2: (props: any) => <Text as="h2" variant="head" {...props} />,
-    h3: (props: any) => <Text as="h3" variant="subhead" {...props} />,
-    h4: (props: any) => <Text as="h3" variant="subhead" {...props} />,
-    span: (props: any) => <Text variant="body" {...props} />,
-    listItem: (props: any) => <Text as="li" {...props} />,
-    block: Block,
-    button: (props: any) => <SanityButton {...props.node} />
-  },
-  listItem: (props: any) => <Text as="li" {...props} />,
-  marks: {
-    link: (props: any) => {
-      return (
-        <Link target={props.mark.targetBlank ? '_blank' : ''} to={props.mark.url}>
-          <Text as="span" color="brand" {...props} />
-        </Link>
-      )
-    },
-
-    colorSanity: (props: any) => (
-      <Text as={props._type} color={props.mark.hex} opacity={props.mark.alpha} {...props} />
-    )
-  }
-}
+import { RichTextBlockContent } from '@/modules/RichText'
 
 const RichTextModule: FC<any> = ({ text }) => (
   <Box
@@ -66,7 +12,7 @@ const RichTextModule: FC<any> = ({ text }) => (
     style={{ textAlign: text.alignment || 'justify' }}
     position="relative"
   >
-    <BlockContent blocks={text.textRaw} serializers={serializers} />
+    <RichTextBlockContent text={text} />
   </Box>
 )
 
