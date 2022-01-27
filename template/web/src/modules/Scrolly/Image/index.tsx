5467b2c52f4bf332efe799ba688356a791ab3dbf
Anton Leitner <anton.b.leitner@gmail.com>
2022-01-27 12:52:32 +0100

    add Scrolly Image in flow functionality

--- template/web/src/modules/Scrolly/Image/index.tsx
+++ template/web/src/modules/Scrolly/Image/index.tsx
@@ -7,16 +7,18 @@ import SanityImage from '@/atoms/SanityImage'
 
 interface ImageModuleProps extends SanityModuleImage {
   background: boolean
+  objectFit?: string
 }
 
-const ImageModule: FC<ImageModuleProps> = ({ image, background }) => {
+const ImageModule: FC<ImageModuleProps> = ({ image, background, objectFit }) => {
   return (
     <Box
+      m={background ? undefined : 5}
       position={background ? 'absolute' : 'relative'}
-      height={background ? '100vh' : undefined}
+      height={background ? '100vh' : '100%'}
       width={background ? '100%' : undefined}
     >
-      <SanityImage image={image} loading="eager" />
+      <SanityImage image={image} loading="eager" objectFit={background ? 'cover' : 'contain'} />
     </Box>
   )
 }
