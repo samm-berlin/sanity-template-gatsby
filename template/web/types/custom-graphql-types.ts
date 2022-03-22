5fb3b7ad13287c9d75bd20283eb0c9fb043d8b96
Anton Leitner <anton.b.leitner@gmail.com>
2022-03-22 13:26:45 +0100

    add types

--- template/web/types/custom-graphql-types.ts
+++ template/web/types/custom-graphql-types.ts
@@ -25,3 +25,41 @@ export type LayoutOptions = {
   height: string
   width: 'narrow' | 'regular' | 'wide'
 }
+
+
+// Video
+
+export type MuxVideoAsset = {
+  _key: string
+  _type: string
+  status: string
+  assetId: string
+  playbackId: string
+  filename: string
+  thumbTime: number
+}
+
+export type MuxVideo = {
+  _key: string
+  _type: string
+  asset: MuxVideoAsset
+}
+
+export type AspectRatio = {
+  _key: string
+  _type: string
+  x?: number
+  y?: number
+}
+
+export type ScrollyModuleMuxVideo = {
+  _key: string
+  _type: string
+  options: ScrollyModuleDefaultFields
+  video: MuxVideo
+  aspectRatio: AspectRatio
+  autoplay: boolean
+  muted: boolean
+  controls: boolean
+  loop: boolean
+}
